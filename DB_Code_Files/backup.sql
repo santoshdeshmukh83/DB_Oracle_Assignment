856cb3ab8fb0fdfdbc76f2307f5817c8d3ab09ba
santoshdeshmukh83 <84096376+santoshdeshmukh83@users.noreply.github.com>
2021-05-12 23:47:12 +0530

    extra semi colon and missing bracket

--- DB_Code_Files/backup.sql
+++ DB_Code_Files/backup.sql
@@ -6,14 +6,13 @@ v_backup_tbl_extn VARCHAR2(20);
 BEGIN
 
    v_backup_tbl_extn := TO_CHAR('YYYYMMDD');
-   FOR i IN (SELECT object_name FROM user_objects WHERE object_type in ('TABLE') LOOP
+   FOR i IN (SELECT object_name FROM user_objects WHERE object_type in ('TABLE')) LOOP
 		BEGIN    
 			  EXECUTE IMMEDIATE 'CREATE TABLE ' || i.object_name||'_'|| v_backup_tbl_extn ||' AS SELECT * FROM ' ||i.object_name ;
-			  ;
 		EXCEPTION
 			  WHEN OTHERS THEN
 				v_error_message := sqlerrm;
-				DBMS_OUTPUT.PUT_LINE('Failed while creating a back up table for '|| i.object_name || ' due to '||  l_error_message);
+				DBMS_OUTPUT.PUT_LINE('Failed while creating a back up table for '|| i.object_name || ' due to '||  v_error_message);
 		END;   
 	END LOOP;	
 END;
